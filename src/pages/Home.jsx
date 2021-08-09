623250ac54c64af2313cdb7c6474eb8eb844cad0
AdiletBekmuratov <dd01x3@gmail.com>
2021-08-09 16:43:07 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CustomLink from '@/components/CustomLink';
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        
+      </main>
+    </>
+  );
+}
