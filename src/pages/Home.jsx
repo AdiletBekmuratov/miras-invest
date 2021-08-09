4a0e05da299e8173e0085c59266cb707208e2690
AdiletBekmuratov <dd01x3@gmail.com>
2021-08-09 23:29:53 +0600

    BadgeCard & fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,22 @@
+import BadgeCard from '@/components/BadgeCard';
 import React from 'react';
 
 export default function Home() {
 	return (
 		<>
 			<main className='bg-white'>
-				<section>
-					<h1 className='text-darkerBlue text-xl'>
-						Компания Mirasinvest был основан в мае 2021 года.
-						Но, не смотря на это, уже успела завоевать доверие рынка и начала сотрудничать с крупными застройщиками Турции.
-					</h1>
-				</section>
+				<div className='max-w-6xl mx-auto px-4 py-10'>
+					<section>
+						<h1 className='text-darkerBlue text-xl'>
+							Компания Mirasinvest был основан в мае 2021 года.
+							Но, не смотря на это, уже успела завоевать доверие рынка и начала сотрудничать с крупными застройщиками Турции.
+						</h1>
+					</section>
+
+					<section className='flex justify-center items-center w-full py-16'>
+						<BadgeCard />
+					</section>
+				</div>
 			</main>
 		</>
 	);
