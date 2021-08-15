dacdbe9630dbe47ec467a9a80d022c489ae4e0b8
AdiletBekmuratov <dd01x3@gmail.com>
2021-08-15 17:26:23 +0600

    remove buggy aos

--- src/components/GradientCard.jsx
+++ src/components/GradientCard.jsx
@@ -8,8 +8,8 @@ function GradientCard({article: { id, image, title, description }, aos}) {
 			<div className="bgcolor opacity-90 absolute w-full h-full top-0 left-0"></div>
 			<img className="w-full h-full object-cover" src={fromImageToUrl(image)} />
 			<div className='absolute bottom-0 left-0 p-8 w-full'>
-				<h2 className="mb-5 text-white text-2xl font-bold" data-aos="fade-right">{title}</h2>
-				<p className="text-white text-base font-medium" data-aos="fade-right" data-aos-delay>{description}</p>
+				<h2 className="mb-5 text-white text-2xl font-bold">{title}</h2>
+				<p className="text-white text-base font-medium">{description}</p>
 			</div>
 		</Link>
 	)
--- src/components/ObjectCard.jsx
+++ src/components/ObjectCard.jsx
@@ -8,9 +8,9 @@ function ObjectCard({ object: { id, image, title, location }, aos }) {
 			<div className="bgcolor opacity-90 absolute w-full h-full top-0 left-0"></div>
 			<img className="object-cover" src={fromImageToUrl(image)} />
 			<div className="absolute bottom-0 left-0 p-8 w-full">
-				<h2 className="text-white text-2xl font-extrabold mb-5 uppercase" data-aos='fade-right'>{title}</h2>
+				<h2 className="text-white text-2xl font-extrabold mb-5 uppercase">{title}</h2>
 				<div className="border-yellowIs border-2 bg-yellowIs mb-5 w-full"></div>
-				<p className="text-white text-2xl font-extrabold" data-aos='fade-right' data-aos-delay="200">{location}</p>
+				<p className="text-white text-2xl font-extrabold" data-aos='fade-right'>{location}</p>
 			</div>
 		</Link>
 	)
