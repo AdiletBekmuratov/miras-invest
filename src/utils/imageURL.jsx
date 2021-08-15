81cdef482a64a584e9c08ccc1eded3f8108c0daf
AdiletBekmuratov <dd01x3@gmail.com>
2021-08-15 16:26:31 +0600

    image fix

--- src/utils/imageURL.jsx
+++ src/utils/imageURL.jsx
@@ -1,7 +1,7 @@
 import Logo from '/images/logo.svg'
 
 export const API_URL = import.meta.env.VITE_APP_API_URL
-export const PUBLIC_URL = import.meta.env.VITE_APP_PUBLIC_URL || "http://localhost:3000"
+export const PUBLIC_URL = import.meta.env.VITE_APP_PUBLIC_URL
 
 export const fromImageToUrl = (image) => {
 	if(!image){
